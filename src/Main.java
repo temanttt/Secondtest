69e24ff2c991154cdebe2748ffb393171e9893b0
Test rep <temanafikov@gmail.com>
2024-07-08 18:08:35 +0300

    First  commit for second test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,6};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
